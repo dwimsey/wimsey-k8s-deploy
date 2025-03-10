3b38934bebe6fa106ec1896eea58db78f8a0814f
David Wimsey <david@wimsey.us>
2025-03-10 11:21:57 -0400

    Very simple initial instructions

--- host-setup/11-gpu-nvidia.sh
+++ host-setup/11-gpu-nvidia.sh
@@ -6,9 +6,3 @@ set -e
 /mnt/software/GPU/nvidia/latest-x86_64-cuda.run --silent -m=kernel-open --no-drm
 
 nvidia-smi
-
-echo When ready run
-echo ''
-echo microk8s enable nvidia --gpu-operator-driver host --gpu-operator-set driver.enabled=false
-echo
-echo 'To enable the nVidia operator'
